071a6afb6cc967cb7964f46e88782286826ccb04
matiasmrtnz <mat.66.martinez@gmail.com>
2023-08-17 11:52:17 -0400

    comentario random

---
+++ EXPLICACION.js
@@ -0,0 +1,13 @@
+/*Comando basicos del git
+
+-git clone  {url del repertorio} : es la forma de clonar en mi pc, el repositorio que está en mi pc
+
+Paso a seguir para subir codigo nuevo a git:
+-git add . : Esto es para agregar los cambios efectuados al commit
+-git commit -m "acá va un comentario" : Es para armar el commit
+- git push : es para enviar el commit
+
+
+
+*/
+
